529213a7b9c18b4f46579c9d43ac7dd59ea9805f
tixity <75755351+tixity@users.noreply.github.com>
2022-10-19 10:41:11 +0300

    first commit

--- lib/widgets/intro_slider.dart
+++ lib/widgets/intro_slider.dart
@@ -73,8 +73,8 @@ class IntroSlider extends StatelessWidget {
           ),
           title: 'Pay your rent instantly',
           body: 'Pay direct from your mpesa or bank account',
-          image: Image.asset(
-            'assets/image2.png',
+          image: Image.network(
+            'https://media.istockphoto.com/photos/modern-scandinavian-living-room-interior-3d-render-picture-id1184204517?b=1&k=20&m=1184204517&s=170667a&w=0&h=5AKfkUCmJun_upYtTCL6mhDJeupe46pdFgbiem-O2-Q=',
             fit: BoxFit.cover,
           ),
         ),
